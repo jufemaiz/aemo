3cbba78a4c3162ee0d6bf57ad10e78fea4c2ee93
Joel Courtney <jcourtney@cozero.com.au>
2014-04-17 17:43:01 +1000

    Region updated to work properly

--- lib/aemo/region.rb
+++ lib/aemo/region.rb
@@ -14,6 +14,8 @@ module AEMO
     def initialize(region)
       if is_valid_region?(region)
         @region = region
+        @current_trading = []
+        @current_dispatch = []
       end
     end
   
@@ -30,14 +32,14 @@ module AEMO
     end
     
     def current_dispatch
-      if @current_dispatch.nil? || @current_dispatch.last.datetime != (Time.now - Time.now.to_i % 300)
+      if @current_dispatch.length == 0 || @current_dispatch.last.datetime != (Time.now - Time.now.to_i % 300)
         @current_dispatch = AEMO::Market.current_dispatch(@region)
       end
       @current_dispatch
     end
     
     def current_trading
-      if @current_trading.nil? || @current_trading.reject{|i| i.period_type != 'TRADING'}.last.datetime != (Time.now - Time.now.to_i % 300)
+      if @current_trading.length == 0 || @current_trading.reject{|i| i.period_type != 'TRADE'}.last.datetime != (Time.now - Time.now.to_i % 300)
         @current_trading = AEMO::Market.current_trading(@region)
       end
       @current_trading
