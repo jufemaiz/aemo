91c1240ad27f964658e37524d64c28a587f43d2c
Christopher Chow <christopher.chow@kinesis.org>
2014-10-10 10:08:10 +1100

    Switch to expect syntax in specs.

--- spec/lib/aemo/market_spec.rb
+++ spec/lib/aemo/market_spec.rb
@@ -3,14 +3,14 @@ require "spec_helper"
 describe AEMO::Market do
   describe '.regions' do
     it "returns the SupportedFormats constant" do
-      AEMO::Market.regions.should == AEMO::Region::REGIONS
+      expect(AEMO::Market.regions).to eq(AEMO::Region::REGIONS)
     end
 
     it "returns the SupportedFormats constant for subclasses" do
       class MyParser < HTTParty::Parser
         SupportedFormats = {"application/atom+xml" => :atom}
       end
-      MyParser.formats.should == {"application/atom+xml" => :atom}
+      expect(MyParser.formats).to eq({"application/atom+xml" => :atom})
     end
   end
 end
--- spec/lib/aemo/region_spec.rb
+++ spec/lib/aemo/region_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 describe AEMO::Region do
   describe '.REGIONS' do
     it "should be a hash" do
-      AEMO::Market.regions.should be_instance_of(Hash)
+      expect(AEMO::Market.regions).to be_instance_of(Hash)
     end
   end
 end
