8f419351c5c54b2adf932aaf111d87fbc81ac413
Joel Courtney <euphemize+git@gmail.com>
2015-02-04 18:42:16 +1100

    MLF Values now included in aemo-tni.json

--- lib/data/xml-to-json.rb
+++ lib/data/xml-to-json.rb
@@ -1,8 +1,18 @@
 require 'nokogiri'
 require 'json'
+require 'csv'
 
 @path  = Dir.pwd
 @files = Dir.entries(@path).reject{|f| %w(. ..).include?(f)}
+
+@mlf_data = {}
+
+# Let's get the CSV Data first
+CSV.open(File.join(@path,"TNI-MLF-Codes.csv"), headers: true, converters: :numeric).each do |row|
+  @mlf_data[row["TNI"]] ||= { location: row['Location'], voltage: row['Voltage'], loss_factors: [] }
+  @mlf_data[row["TNI"]][:loss_factors] << { start: Date.parse('2014-07-01'), finish: Date.parse('2015-06-30'), value: row['FY15']}
+  @mlf_data[row["TNI"]][:loss_factors] << { start: Date.parse('2013-07-01'), finish: Date.parse('2014-06-30'), value: row['FY14']}
+end
 
 # Let's do TNI First
 @files.select{|x| ['aemo-tni.xml','aemo-dlf.xml'].include?(x) }.each do |file|
@@ -19,6 +29,9 @@ require 'json'
     output_data_instance = {}
     row_children.each do |row_child|
       output_data_instance[row_child.name] = row_child.children.first.text
+    end
+    if file.match(/tni/)
+      output_data_instance[:mlf_data] = @mlf_data[code]
     end
     output_data[code] << output_data_instance
   end
