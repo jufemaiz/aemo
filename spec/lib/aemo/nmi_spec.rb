0cace96b7f0e140506805c1439b8dac1dd0daf2c
Joel Courtney <jcourtney@cozero.com.au>
2016-05-15 15:58:57 +1000

    Revised pull request

--- spec/lib/aemo/nmi_spec.rb
+++ spec/lib/aemo/nmi_spec.rb
@@ -27,23 +27,27 @@ describe AEMO::NMI do
   # CLASS METHODS
   # ---
   describe '.valid_nmi?(nmi)' do
-    it 'should validate nmi' do
-      json.each do |nmi|
-        expect(AEMO::NMI.valid_nmi?(nmi['nmi'])).to eq(true)
+    context 'valid' do
+      it 'should validate nmi' do
+        json.each do |nmi|
+          expect(AEMO::NMI.valid_nmi?(nmi['nmi'])).to eq(true)
+        end
       end
     end
-    it 'should invalidate' do
-      expect(AEMO::NMI.valid_nmi?('OOOOOOOOOO')).to eq(false)
-    end
-    it 'should invalidate' do
-      expect(AEMO::NMI.valid_nmi?('NM100')).to eq(false)
-    end
-    it 'should invalidate' do
-      expect { AEMO::NMI.valid_nmi? }.to raise_error(ArgumentError)
+    context 'invalid' do
+      it 'should invalidate' do
+        expect(AEMO::NMI.valid_nmi?('OOOOOOOOOO')).to eq(false)
+      end
+      it 'should invalidate' do
+        expect(AEMO::NMI.valid_nmi?('NM100')).to eq(false)
+      end
+      it 'should invalidate' do
+        expect { AEMO::NMI.valid_nmi? }.to raise_error(ArgumentError)
+      end
     end
   end
 
-  describe '.self.valid_checksum?(nmi, checksum)' do
+  describe '.valid_checksum?(nmi, checksum)' do
     it 'should validate valid nmi and checksums' do
       json.each do |nmi|
         expect(AEMO::NMI.valid_checksum?(nmi['nmi'], nmi['checksum'])).to eq(true)
@@ -51,7 +55,7 @@ describe AEMO::NMI do
     end
   end
 
-  describe '.self.network?(nmi)' do
+  describe '.network?(nmi)' do
     it 'should return a network for an allocated NMI' do
       network = AEMO::NMI.network('NCCCC00000')
       expect(network.to_a[0].last[:title]).to eq('Ausgrid')
@@ -66,14 +70,24 @@ describe AEMO::NMI do
   # INSTANCE METHODS
   # ---
   describe '#initialize' do
-    it 'should raise an ArgumentError error' do
-      expect { AEMO::NMI.new('OOOOOOOOOO') }.to raise_error(ArgumentError)
+    context 'valid' do
+      it 'should return a valid NMI' do
+        expect(AEMO::NMI.new('NM10000000')).to be_a(AEMO::NMI)
+      end
+      it 'should return a valid NMI with MSATS' do
+        expect(AEMO::NMI.new('NM10000000', { msats_detail: {} })).to be_a(AEMO::NMI)
+      end
     end
-    it 'should raise an ArgumentError error' do
-      expect { AEMO::NMI.new('NM100') }.to raise_error(ArgumentError)
-    end
-    it 'should raise an ArgumentError error' do
-      expect { AEMO::NMI.new }.to raise_error(ArgumentError)
+    context 'invalid' do
+      it 'should raise an ArgumentError error' do
+        expect { AEMO::NMI.new('OOOOOOOOOO') }.to raise_error(ArgumentError)
+      end
+      it 'should raise an ArgumentError error' do
+        expect { AEMO::NMI.new('NM100') }.to raise_error(ArgumentError)
+      end
+      it 'should raise an ArgumentError error' do
+        expect { AEMO::NMI.new }.to raise_error(ArgumentError)
+      end
     end
   end
   describe '#valid_nmi?' do
